31d29f928a8be3e440a703affebd5cf9d5b40d74
ahammed shabas <ahammedshabas.k@eyerov.com>
2025-05-22 23:37:56 +0530

    completed task

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-const allowedOrigins = ['http://localhost:5174', 'http://localhost:3000'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
 
 app.use(cors({
   origin: function (origin, callback) {
